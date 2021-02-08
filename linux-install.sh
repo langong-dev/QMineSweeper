5f9acce25a53f965eadaaad8f4fcb3a7317dd207
5+1 <59787082+langonginc@users.noreply.github.com>
2021-02-08 19:26:49 +0800

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -2,12 +2,22 @@
 
 echo ' LanGong LGame QMineSweeper Install Script'
 
+download () {
+  echo ' >> Download'
+  echo '    -> (0/2) Downloading source from GitHub'
+  git clone https://github.com/langong-dev/QMineSweeper.git "$HOME/.qms-cache"
+  echo '    -> (1/2) Loading source'
+  cd "$HOME/.qms-cache"
+  echo '    -> (2/2) Done!'
+}
+
 compile (){
   echo ' >> Compile'
-  echo '    -> (0/1) Compile'
-  qmake ScreenShot.pro
+  echo '    -> (0/2) Qt Making'
+  qmake QMineSweeper.pro
+  echo '    -> (1/2) C Making'
   make
-  echo '    -> (1/1) Done!'
+  echo '    -> (2/2) Done!'
 }
 
 installto (){
@@ -15,16 +25,17 @@ installto (){
   dir="$2"
   echo ' >> Install'
   echo '    -> (0/1) Install bin to "'$bin'"'
-  cp ScreenShot "$bin/screenshotl" || cp bin/ScreenShot "$bin/screenshotl"
-  cp -r ./* "$dir/screenshotl"
-  cp ./screenshotl.desktop "$dir/applications"
+  cp QMineSweeper "$bin/qminesweeper" || cp bin/QMineSweeper "$bin/qminesweeper"
+  cp -r ./* "$dir/qminesweeper"
   echo '    -> (1/1) Done!'
 }
 
 if [ $1 == 'dir' ]; then
+  download
   compile
   installto "$2" "$3"
 else
+  download
   compile
   installto "/usr/bin" "/usr/share"
 fi
